250f8b0b36df6c2e62aa717167b6d9904db65bc6
Alberto Donato <alberto.donato@canonical.com>
2023-04-17 07:38:25 +0000

    fix typo in var name for POSTGRES_HOST in data import script

--- test-data/import.sh
+++ test-data/import.sh
@@ -3,7 +3,7 @@
 # This script imports the testdata from this directory into the postgres database
 #
 
-POSTGRES_HOST="${POSTGRERS_HOST:-localhost}"
+POSTGRES_HOST="${POSTGRES_HOST:-localhost}"
 POSTGRES_PORT="${POSTGRES_PORT:-5432}"
 POSTGRES_DB="${POSTGRES_DB:-postgres}"  # default for postgres docker image
 POSTGRES_USER="${POSTGRES_USER:-postgres}"  # default for postgres docker image
