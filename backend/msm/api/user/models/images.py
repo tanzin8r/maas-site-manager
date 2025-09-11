6c83b438eb6ed0cda89860d76399cce2fc02fe60
Wyatt Rees <wyatt.rees@canonical.com>
2025-09-11 14:42:43 +0000

    refactor(be-images): Reorganize selections endpoints to have their own file

--- backend/msm/api/user/models/images.py
+++ backend/msm/api/user/models/images.py
@@ -1,38 +1,9 @@
 from typing import Self
 
-from pydantic import BaseModel
-
 from msm.db import models
-
-
-class GetSelectableImagesResponse(BaseModel):
-    items: list[models.SelectableImage]
-
-
-class GetSelectedImagesResponse(BaseModel):
-    items: list[models.SelectedImage]
 
 
 class ImagesPostResponse(models.BootAssetItem):
     @classmethod
     def from_model(cls, model: models.BootAssetItem) -> Self:
         return cls(**model.model_dump())
-
-
-class ImageSource(BaseModel):
-    selection_id: int
-    id: int
-    name: str
-    url: str
-
-
-class GetImageSourcesResponse(BaseModel):
-    items: list[ImageSource]
-
-
-class SelectImagesPostRequest(BaseModel):
-    selection_ids: list[int]
-
-
-class RemoveSelectedImagesPostRequest(BaseModel):
-    selection_ids: list[int]
